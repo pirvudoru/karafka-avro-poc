3330f6a245d5c813b60c0093ef872ffe6b5b1a8e
pirvudoru <pirvu.doru@ymail.com>
2019-08-26 13:47:40 +0300

    Update waterdrop settings

--- config/initializers/waterdrop.rb
+++ config/initializers/waterdrop.rb
@@ -1,4 +1,6 @@
 WaterDrop.setup do |config|
   config.deliver = !Rails.env.test?
   config.kafka.seed_brokers = [ENV['KAFKA_HOST'] || 'kafka://localhost:9092']
+  config.kafka.delivery_threshold = 1_000
+  config.kafka.max_queue_size = 1_000_000
 end
